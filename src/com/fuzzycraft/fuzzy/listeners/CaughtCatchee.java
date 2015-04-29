2e4c64e7b68b122483e117f00cb9247d20a8eda1
FuzzyStatic <af732@drexel.edu>
2015-04-29 13:51:34 -0400

    Caught Catchee

---
+++ src/com/fuzzycraft/fuzzy/listeners/CaughtCatchee.java
@@ -0,0 +1,28 @@
+package com.fuzzycraft.fuzzy.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import com.fuzzycraft.fuzzy.Catchee;
+
+public class CaughtCatchee implements Listener {
+
+	private Catchee catchee;
+		
+	public CaughtCatchee(Catchee catchee) {
+		this.catchee = catchee;
+	}
+	
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+		Player catcher = event.getPlayer();
+
+		if (event.getRightClicked() instanceof Player && event.getRightClicked().equals(this.catchee.getCatchee())) {
+			this.catchee.timer();
+			System.out.println(event.getRightClicked().getName());
+		}
+	}
+}
