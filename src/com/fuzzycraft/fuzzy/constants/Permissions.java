11c0007b089618805979ca4b3fb839ca20f48e11
FuzzyStatic <af732@drexel.edu>
2015-05-03 17:15:42 -0400

    File header

--- src/com/fuzzycraft/fuzzy/constants/Permissions.java
+++ src/com/fuzzycraft/fuzzy/constants/Permissions.java
@@ -1,5 +1,11 @@
 package com.fuzzycraft.fuzzy.constants;
 
+/**
+ * 
+ * @author FuzzyStatic (fuzzy@fuzzycraft.com)
+ *
+ */
+
 public class Permissions {
 	public static final String CATCHEE = "catchme.catchee";
 	public static final String CATCHER = "catchme.catcher";
